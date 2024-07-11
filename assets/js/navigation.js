31950662130a73eb5494a50dd8de5be0729e7fd8
Daniel-Digid <daniel@dig.id>
2024-07-11 13:26:42 +0100

    d

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -4,8 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   //wait until images, links, fonts, stylesheets, and js is loaded
   window.addEventListener("load", () => {
 
-    
+    /* Hamburguer toggle */
+    const $toggleBtn = $('.menu-toggle')
+    $toggleBtn.on('click', (e) => {
 
+
+      if( $('#header-main').hasClass( 'menu-open' ) ) {
+        $('#header-main').removeClass('menu-open');
+        $('body').css('overflow', 'auto');
+        $('.menu-wrapper').slideUp(700);
+      } else {
+        $('#header-main').addClass('menu-open');
+        $('body').css('overflow', 'hidden');
+        $('.menu-wrapper').slideDown(700);
+      }
+    });
 
   }, false);
 });
