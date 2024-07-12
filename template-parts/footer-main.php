4520c98f7405680bdd9a5a6714f630d1e342c9d5
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-07-12 17:56:52 +0100

    up

--- template-parts/footer-main.php
+++ template-parts/footer-main.php
@@ -19,7 +19,7 @@
 			logo partner
 		</div>
 	</section>
-	<section class="bg-blue-shade-5 text-blue-shade-1 py-32 text-body">
+	<section class="footer-bunchen bg-blue-shade-5 text-blue-shade-1 py-32 text-body">
 		<div class="max-w-xl flex flex-col justify-center items-center mx-auto">
 			<h4 class="text-title mb-8">Superior Komfort mit Flussblick</h4>
 			<div class="flex justify-between items-center">
@@ -27,7 +27,24 @@
 				<a href="" class="btn-internal btn-internal--shade-3">Jetzt Buchen</a>
 			</div>
 		</div>
-
 	</section>
-
+	<section class="footer-copyright bg-blue-shade-4 text-body text-blue-shade-1 z-10">
+		<div class="flex justify-between py-4 max-w-7xl mx-auto">
+			<p>
+			<?php
+				$y = date( 'Y' );
+				printf(
+					esc_html__( 'Urheberrecht &copy; %d Hotel an der den Aare', 'aare' ),
+					esc_html( $y )
+				);
+				?>
+			</p>
+			<a href="#header-main" class="flex">
+				<?php esc_html_e( 'Zurück nach oben', 'aare' ); ?>
+				<svg width="11" height="14" viewBox="0 0 11 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="ml-2">
+					<path d="M5.5 1L1 6M5.5 1L10 6M5.5 1V13.5" stroke="#F2FAFF"/>
+				</svg>
+			</a>
+		</div>
+	</section>
 </footer>
