a00042e92fb14b57729105c7a5b8ccc1c163a8ca
Daniel-Digid <daniel@dig.id>
2024-08-20 15:52:19 +0100

    d

--- template-parts/header-main.php
+++ template-parts/header-main.php
@@ -1,8 +1,8 @@
-<header id="header-main" class="header-main bg-blue-shade-5 absolute top-0 left-0 w-full z-50 overflow-hidden" itemscope itemtype="http://schema.org/WebSite">
+<header id="header-main" class="header-main bg-transparent absolute top-0 left-0 w-full z-50 overflow-hidden" itemscope itemtype="http://schema.org/WebSite">
 
 	<nav class="navbar relative overflow-hidden" role="navigation" aria-label="<?php esc_attr_e( 'Main menu', 'aare' ); ?>">
-		<div class="header-wrapper grid grid-cols-3 px-6 md:px-8 xl:px-16 py-6 md:py-8 xl:py-6">
-			<div class="col-span-1 flex justify-start items-center">
+		<div class="header-wrapper relative grid grid-cols-3 px-6 md:px-8 xl:px-16 py-6 md:py-8 xl:py-6 z-40 md:z-auto">
+			<div class="col-span-1 flex justify-end md:justify-start items-center order-3 md:order-1 md:z-50">
 				<div class="menu-toggle-wrapper">
 					<button class="menu-toggle">
 						<span class="menu-toggle__bars">
@@ -13,15 +13,15 @@
 						<span class="menu-toggle__text"><?php esc_html_e( 'Menu', 'aare' ); ?></span>
 					</button>
 				</div>
-				<?php do_action( 'wpml_add_language_selector' ); ?>
+				<div class="hidden md:block"><?php do_action( 'wpml_add_language_selector' ); ?></div>
 			</div>
-			<div class="col-span-1 flex justify-center items-center">
+			<div class="col-span-1 col-start-2 flex justify-center items-center order-2">
 				<div class="site-branding">
 					<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url" class="navbar-brand custom-logo-link"><?php do_action( 'theme_logo' ); ?></a>
 				</div>
 			</div>
-			<div class="col-span-1 flex justify-end items-center gap-x-8">
-				<a class="btn-internal btn-internal--shade-3 !text-[16px] !font-poppins uppercase font-medium tracking-[0.16px] !px-8 !py-[0.35rem]"  href="#">Jetzt Buchen</a>
+			<div class="col-span-1 hidden md:flex justify-end items-center gap-x-8 order-3">
+				<a class="btn-internal btn-internal--shade-3 !text-[16px] !font-poppins uppercase font-medium tracking-[0.16px] !px-8 !py-[0.35rem] !bg-transparent"  href="#">Jetzt Buchen</a>
 			</div>
 		</div>
 		<div class="px-6 md:px-8 xl:px-16 py-6 md:py-8 xl:py-3 border-t-[0.5px] border-b-[0.5px] border-blue-shade-2 border-opacity-80">
@@ -62,10 +62,7 @@
 					);
 					?>
 				</div>
-				<div class="col-span-12 pl-8 pt-12 block xl:hidden mobile__nav">
-					<div class="site-branding mega-menu__logo">
-						<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url" class="navbar-brand custom-logo-link"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/mega-menu__logo.svg" alt="Hotel Aaren Logo" title="Hotel Aaren Logo" /></a>
-					</div>
+				<div class="col-span-12 px-0 py-0 md:pl-8 md:pt-12 bg-blue-shade-5 block xl:hidden mobile__nav">
 					<nav>
 						<?php
 						wp_nav_menu(
@@ -73,7 +70,7 @@
 								'theme_location'  => 'main-mega-menu',
 								'menu_id'         => 'main-menu',
 								'container_class' => 'main-menu-container pt-20 md:pt-0',
-								'menu_class'      => 'main-menu-top-level relative',
+								'menu_class'      => 'main-menu-top-level p-8',
 								'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
 								'fallback_cb'     => '__return_false',
 								'walker'          => new Custom_Walker_Nav_Menu(),
--- template-parts/pages/home/hero.php
+++ template-parts/pages/home/hero.php
@@ -1,3 +1,30 @@
-<section class="section-hero bg-dark-blue pt-80 pb-20 xl:pt-[154px] xl:pb-28 relative">
-	test text
+<section class="section-hero bg-blue-shade-1 pt-0 pb-0 relative">
+	<div class="hero-block relative">
+		<div class="absolute top-0 left-0 w-full h-[80vh]" style="background: linear-gradient(180deg, rgba(14, 50, 74, 0.85) 0%, rgba(232, 246, 255, 0.85) 48.5%);mix-blend-mode: multiply;"></div>
+		<?php 
+			$hero_image = get_field('hero_section_hero_image');
+			$size = 'full';
+			if( $hero_image ) {
+				echo wp_get_attachment_image( $hero_image, $size, false, array( 'class' => 'w-full' ) );
+			} 
+		?>
+		<div class="absolute bottom-0 left-0 w-full h-[35vh]" style="background: linear-gradient(180deg, rgba(14, 50, 74, 0.00) 0%, #0E324A 100%);"></div>
+	</div>
+	<div class="hero-title text-blue-shade-2 bg-blue-shade-5 -mt-[3px] pb-12">
+		<div class="grid grid-cols-12">
+			<div class="col-span-3 flex items-center justify-end">
+				<p class="font-miller font-normal text-sm xl:text-2xl"><?php echo get_field( 'hero_section_before_title' ); ?></p>
+			</div>
+			<div class="col-span-6 text-center">
+				<p class="text-hero-title pb-6"><?php echo get_field( 'hero_section_title' ); ?></p>
+			</div>
+			<div class="col-span-3 flex items-center justify-start">
+				<p class="font-miller font-normal text-sm xl:text-2xl"><?php echo get_field( 'hero_section_after_title' ); ?></p>
+			</div>
+		</div>
+		<div class="w-full text-center pt-7">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/paper-boat.svg" class="mx-auto" alt="Paper boat" title="Paper boat" />
+		</div>
+	</div>
 </section>
+<hr class="wave-separator border-0" />
