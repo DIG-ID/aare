e0a9ffa91955e51f068b47db20a33763cd7cbcf4
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-09-06 16:24:51 +0100

    up

--- template-parts/components/teaser-default.php
+++ template-parts/components/teaser-default.php
@@ -1,28 +1,28 @@
 <?php
-if( have_rows('teasers_boxes') ):
-    while( have_rows('teasers_boxes') ) : the_row(); ?>
-    <div class="col-span-2 xl:col-span-6 grid grid-cols-2 xl:grid-cols-6 gap-x-6 mt-32 xl:mt-0">
-        <div class="col-span-2 xl:col-span-4 col-start-1 xl:col-start-2">
-            <?php 
-                $teasers_image = get_sub_field('image');
-                $size = 'full';
-                if( $teasers_image ) {
-                    echo wp_get_attachment_image( $teasers_image, $size, false, array( 'class' => 'w-full rounded-[20px]' ) );
-                } 
-            ?>
-            <h3 class="text-title-h3 text-blue-shade-5 mt-10 mb-6"><?php echo get_sub_field( 'title' ); ?></h3>
-            <p class="text-body !leading-[26px] text-blue-shade-5 mb-8"><?php echo get_sub_field( 'text' ); ?></p>
-            <?php 
-            $teasers_btn = get_sub_field('button');
-            if( $teasers_btn ): 
-                $teasers_btn_url = $teasers_btn['url'];
-                $teasers_btn_title = $teasers_btn['title'];
-                $teasers_btn_target = $teasers_btn['target'] ? $teasers_btn['target'] : '_self';
-                ?>
-                <a class="btn-internal btn-internal--shade-transparent text-center" href="<?php echo esc_url( $teasers_btn_url ); ?>" target="<?php echo esc_attr( $teasers_btn_target ); ?>"><?php echo esc_html( $teasers_btn_title ); ?></a>
-            <?php endif; ?>
-        </div>
-    </div>
-    <?php endwhile;
+if ( have_rows( 'teasers_boxes' ) ) :
+	while ( have_rows( 'teasers_boxes' ) ) : the_row(); ?>
+	<div class="col-span-2 xl:col-span-6 grid grid-cols-2 xl:grid-cols-6 gap-x-6 mt-32 xl:mt-0">
+		<div class="col-span-2 xl:col-span-4 col-start-1 xl:col-start-2">
+			<?php
+			$teasers_image = get_sub_field('image');
+			$size = 'full';
+			if ( $teasers_image ) {
+				echo wp_get_attachment_image( $teasers_image, $size, false, array( 'class' => 'w-full rounded-[20px]' ) );
+			}
+			?>
+			<h3 class="text-title-h3 text-blue-shade-5 mt-10 mb-6"><?php echo get_sub_field( 'title' ); ?></h3>
+			<p class="text-body !leading-[26px] text-blue-shade-5 mb-8"><?php echo get_sub_field( 'text' ); ?></p>
+			<?php 
+			$teasers_btn = get_sub_field( 'button' );
+			if ( $teasers_btn ) :
+				$teasers_btn_url    = $teasers_btn['url'];
+				$teasers_btn_title  = $teasers_btn['title'];
+				$teasers_btn_target = $teasers_btn['target'] ? $teasers_btn['target'] : '_self';
+				?>
+				<a class="btn-internal btn-internal--shade-transparent text-center" href="<?php echo esc_url( $teasers_btn_url ); ?>" target="<?php echo esc_attr( $teasers_btn_target ); ?>"><?php echo esc_html( $teasers_btn_title ); ?></a>
+			<?php endif; ?>
+		</div>
+	</div>
+	<?php endwhile;
 endif;
 ?>
--- template-parts/pages/celebrations-conferences/content.php
+++ template-parts/pages/celebrations-conferences/content.php
@@ -1,3 +1,49 @@
-<section>
-	<h2>celebrations & conferences content</h2>
+<section class="section-celebrations-conference-content pb-28">
+	<div class="theme-container">
+		<div class="theme-grid">
+			<?php
+			$intro = get_field( 'intro' );
+			if ( $intro ) :
+				?>
+				<div class="col-span-12 card-cs w-full bg-white overflow-hidden rounded-[20px] order-5 lg:order-3">
+					<?php
+					$iimage = get_field( 'intro_image' );
+					if ( $iimage ) :
+						echo wp_get_attachment_image( $iimage, 'full', false, array( 'class' => 'w-full object-fit rounded-b-[20px] min-h-[116px] lg:min-h-[225px]' ) );
+					endif;
+					?>
+					<div class="card-cs-content py-7 lg:py-20 px-9 lg:px-28 grid grid-cols-2 lg:grid-cols-12">
+						<div class="col-span-2 lg:col-span-4 lg:col-start-2 mb-14 lg:mb-0">
+							<h2 class="text-title-h3 text-blue-shade-5 mb-4 lg:mb-7"><?php the_field( 'intro_title_1' ); ?></h2>
+							<div class="text-body text-blue-shade-5"><?php the_field( 'intro_description_1' ); ?></div>
+						</div>
+						<div class="col-span-2 lg:col-span-4 lg:col-start-8 mb-14 lg:mb-0">
+							<h2 class="text-title-h3 text-blue-shade-5 mb-4 lg:mb-7"><?php the_field( 'intro_title_2' ); ?></h2>
+							<div class="text-body text-blue-shade-5 mb-9"><?php the_field( 'intro_description_2' ); ?></div>
+							<?php
+							$ilink = get_field( 'intro_link' );
+							if ( $ilink ) :
+									$link_url    = $ilink['url'];
+									$link_title  = $ilink['title'];
+									$link_target = $ilink['target'] ? $ilink['target'] : '_self';
+									?>
+									<a class="btn-external btn-external--shade-1" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+							<?php
+							endif;
+							?>
+						</div>
+					</div>
+				</div>
+				<?php
+			endif;
+			?>
+		</div>
+	</div>
+</section>
+<section class="section-page-teasers pb-32 lg:py-36">
+	<div class="theme-container">
+		<div class="theme-grid">
+			<?php get_template_part( 'template-parts/components/teaser-default' ); ?>
+		</div>
+	</div>
 </section>
