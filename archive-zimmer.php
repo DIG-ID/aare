8d0caacbbc6703cf54d0d937427c8d144c377f9a
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-09-13 15:12:59 +0100

    up

--- archive-zimmer.php
+++ archive-zimmer.php
@@ -1,7 +1,7 @@
 <?php
 get_header();
 do_action( 'before_main_content' );
-get_template_part( 'template-parts/archives/archives-header' );
+get_template_part( 'template-parts/archives/zimmer/header' );
 get_template_part( 'template-parts/archives/zimmer/content' );
 get_template_part( 'template-parts/archives/zimmer/informations' );
 do_action( 'after_main_content' );
--- template-parts/posts/post-content.php
+++ template-parts/posts/post-content.php
@@ -1,8 +1,8 @@
-<section class="section-post-content bg-blue-shade-5 text-blue-shade-2 lg:pt-28 lg:pb-28 pt-10">
+<section class="section-post-content bg-blue-shade-5 lg:pt-28 lg:pb-28 pt-10">
 	<div class="theme-container">
 		<div class="theme-grid">
 			<div class="col-span-2 lg:col-span-6 lg:col-start-2">
-				<h1 class="text-title mb-6"><?php the_title(); ?></h1>
+				<h1 class="text-title text-blue-shade-5 mb-6"><?php the_title(); ?></h1>
 				<div class="section-post-content-wrapper">
 					<?php the_content(); ?>
 				</div>
