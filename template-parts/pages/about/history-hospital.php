c7c5230287a8633aa07e2b93643be767bec7d5d0
Daniel-Digid <daniel@dig.id>
2024-09-18 14:06:43 +0100

    d

--- template-parts/pages/about/history-hospital.php
+++ template-parts/pages/about/history-hospital.php
@@ -1,67 +1,130 @@
-<section class="section-history-hospital bg-blue-shade-4 relative overflow-hidden">
-    
-    <div class="history-hospital py-12 relative">
-        
-        <div class="h-[90vh] w-[4925px] grid grid-cols-9 my-auto bg-blue-shade-4">
-            <div class="col-span-1 bg-blue-shade-4 text-right">
+<section class="section-history-hospital relative overflow-hidden">
+    <div class="history-hospital py-12 relative pr-20">    
+        <div class="h-[90vh] w-[6000px] grid grid-cols-10 my-auto">
+            <div class="col-span-1 text-right">
             <?php 
-                $banner_image = get_field('hospital_banner');
-                $size = 'full';
-                if( $banner_image ) {
-                    echo wp_get_attachment_image( $banner_image, $size, false, array( 'class' => 'w-[300px] rounded-[24px] inline-block mr-6' ) );
-                } 
+            $banner_image = get_field('hospital_banner');
+            $banner_image_hover = get_field('hospital_banner_hover');
+            $size = 'full';
+            if( $banner_image && $banner_image_hover ) { 
             ?>
+                <div class="image-container relative">
+                    <?php echo wp_get_attachment_image( $banner_image, $size, false, array( 'class' => 'hospital-image w-[300px] rounded-[24px] inline-block mr-6 absolute top-0 right-0 transition-opacity duration-700 ease-in-out opacity-100' ) ); ?>
+                    <?php echo wp_get_attachment_image( $banner_image_hover, $size, false, array( 'class' => 'hospital-image-hover w-[300px] rounded-[24px] inline-block mr-6 absolute top-0 right-0 transition-opacity duration-700 ease-in-out opacity-0' ) ); ?>
+                </div>
+            <?php
+            } 
+            ?>
+
             </div>
-            <div class="col-span-1 pl-28 pt-20 bg-blue-shade-1 rounded-l-[24px]">
+            <div class="col-span-1 pl-28 pt-20 bg-blue-shade-1 rounded-l-[24px] relative">
                 <h2 class="font-miller font-light text-2xl xl:text-5xl leading-7 w-[60%] text-blue-shade-5"><?php echo get_field( 'hospital_title' ); ?></h2>
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/waves.svg" class="w-[143px] py-11" alt="waves" title="waves" />
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_1_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-32 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_1_description' ); ?></p>
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_2_year' ); ?></h3>
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 mt-0 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_1_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-[22%] lg:max-w-[410px]"><?php the_field( 'hospital_timeline_1_description' ); ?></p>
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_2_year' ); ?></h3>
                 <p class="text-body text-blue-shade-5 mb-0 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_2_description' ); ?></p>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/paper-boat.svg" class="w-[66px] absolute bottom-[10%] left-0 ml-28" alt="paper boat" title="paper boat" />
+                <div class="w-full absolute bottom-[8%] right-0 pl-28 z-10"><div class="line-with-dots"></div></div>
             </div>
-            <div class="col-span-1 pl-8 pt-20 bg-blue-shade-1 lg:-ml-[2px]">
-                Image
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_3_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-32 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_3_description' ); ?></p>
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_4_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-0 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_4_description' ); ?></p>
+            <div class="col-span-1 pt-20 bg-blue-shade-1 lg:-ml-[2px] relative">
+                <?php
+                $himage1 = get_field( 'hospital_image_1' );
+                if ( $himage1 ) :
+                    echo wp_get_attachment_image( $himage1, 'full', false, array( 'class' => 'w-full object-cover rounded-[24px] lg:max-w-[650px] lg:min-w-[650px] lg:-mt-[20%] lg:-ml-[18%] lg:mb-[7%]' ) );
+                endif;
+                ?>
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_3_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-[5%] lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_3_description' ); ?></p>
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_4_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-0 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_4_description' ); ?></p>
+                <div class="w-full absolute bottom-[8%] right-0 z-10"><div class="line-single-dot"></div></div>
             </div>
-            <div class="col-span-1 pl-8 pt-20 bg-blue-shade-1 lg:-ml-[2px]">
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_5_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-20 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_5_description' ); ?></p>
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_6_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-32 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_6_description' ); ?></p>
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_7_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-0 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_7_description' ); ?></p>
+            <div class="col-span-1 pt-20 bg-blue-shade-1 lg:-ml-[2px] relative">
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_5_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-[11%] lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_5_description' ); ?></p>
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_6_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-[18%] lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_6_description' ); ?></p>
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_7_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-0 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_7_description' ); ?></p>
+                <div class="w-full absolute bottom-[8%] right-0 z-10"><div class="line-single-dot"></div></div>
             </div>
-            <div class="col-span-1 pl-8 pt-20 bg-blue-shade-1 lg:-ml-[2px]">
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_8_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-8 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_8_description' ); ?></p>
-                Image
+            <div class="col-span-1 pt-20 bg-blue-shade-1 lg:-ml-[2px] relative">
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_8_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-[5%] lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_8_description' ); ?></p>
+                <?php
+                $himage2 = get_field( 'hospital_image_2' );
+                if ( $himage2 ) :
+                    echo wp_get_attachment_image( $himage2, 'full', false, array( 'class' => 'w-full object-cover rounded-[24px] mx-auto lg:max-w-[520px] lg:mb-0' ) );
+                endif;
+                ?>
+                <div class="w-full absolute bottom-[8%] right-0 z-10"><div class="line-single-dot"></div></div>
             </div>
-            <div class="col-span-1 pl-8 pt-20 bg-blue-shade-1 lg:-ml-[2px]">
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_9_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-20 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_9_description' ); ?></p>
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_10_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-28 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_10_description' ); ?></p>
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_11_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-0 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_11_description' ); ?></p>
+            <div class="col-span-1 pt-20 bg-blue-shade-1 lg:-ml-[2px] relative">
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_9_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-[14%] lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_9_description' ); ?></p>
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_10_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-[15%] lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_10_description' ); ?></p>
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_11_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-0 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_11_description' ); ?></p>
+                <div class="w-full absolute bottom-[8%] right-0 z-10"><div class="line-single-dot"></div></div>
             </div>
-            <div class="col-span-1 pl-8 pt-20 bg-blue-shade-1 lg:-ml-[2px]">Image 1<br>Image 2</div>
-            <div class="col-span-1 pl-8 pt-20 bg-blue-shade-1 lg:-ml-[2px]">
-            <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_12_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-20 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_12_description' ); ?></p>
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_13_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-28 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_13_description' ); ?></p>
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_14_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-0 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_14_description' ); ?></p>
+            <div class="col-span-1 pt-20 bg-blue-shade-1 lg:-ml-[2px] relative">
+            <?php
+                $himage3 = get_field( 'hospital_image_3' );
+                if ( $himage3 ) :
+                    echo wp_get_attachment_image( $himage3, 'full', false, array( 'class' => 'w-full object-cover rounded-[24px] mx-auto lg:max-w-[520px] mb-10 lg:mb-[9%] lg:-mt-[20%]' ) );
+                endif;
+                ?>
+                <?php
+                $himage4 = get_field( 'hospital_image_4' );
+                if ( $himage4 ) :
+                    echo wp_get_attachment_image( $himage4, 'full', false, array( 'class' => 'w-full object-cover rounded-[24px] mx-auto lg:max-w-[520px] lg:mb-0' ) );
+                endif;
+                ?>
+                <div class="w-full absolute bottom-[8%] right-0 z-10"><div class="line-single-dot"></div></div>
             </div>
-            <div class="col-span-1 pl-8 pt-20 bg-blue-shade-1 lg:-ml-[2px]">
-                <h3 class="text-title-h3 text-blue-shade-5 mb-4"><?php the_field( 'hospital_timeline_15_year' ); ?></h3>
-                <p class="text-body text-blue-shade-5 mb-8 lg:max-w-[410px]"><?php the_field( 'hospital_timeline_15_description' ); ?></p>
-                Image
+            <div class="col-span-1 pt-20 bg-blue-shade-1 lg:-ml-[2px] relative">
+            <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_12_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-[21%] lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_12_description' ); ?></p>
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_13_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-[18%] lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_13_description' ); ?></p>
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_14_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-0 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_14_description' ); ?></p>
+                <div class="w-full absolute bottom-[8%] right-0 z-10"><div class="line-single-dot"></div></div>
+            </div>
+            <div class="col-span-1 pt-20 bg-blue-shade-1 lg:-ml-[2px] relative">
+                <h3 class="text-title-h3 text-blue-shade-5 mb-4 lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_15_year' ); ?></h3>
+                <p class="text-body text-blue-shade-5 mb-[22%] lg:max-w-[410px] mx-auto"><?php the_field( 'hospital_timeline_15_description' ); ?></p>
+                <?php
+                $himage5 = get_field( 'hospital_image_5' );
+                if ( $himage5 ) :
+                    echo wp_get_attachment_image( $himage5, 'full', false, array( 'class' => 'w-full object-cover rounded-[24px] mx-auto lg:max-w-[520px] lg:mb-0' ) );
+                endif;
+                ?>
+                <div class="w-full absolute bottom-[8%] right-0 z-10"><div class="line-single-dot"></div></div>
+            </div>
+            <div class="col-span-1 pt-20 lg:-ml-[2px] bg-blue-shade-1 rounded-r-[24px] w-1/3">
+
             </div>
         </div>
     </div>
 </section>
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    const image = document.querySelector('.hospital-image');
+    const imageHover = document.querySelector('.hospital-image-hover');
+    
+    if (image && imageHover) {
+        setInterval(() => {
+        if (image.style.opacity == '0') {
+            image.style.opacity = '1';
+            imageHover.style.opacity = '0';
+        } else {
+            image.style.opacity = '0';
+            imageHover.style.opacity = '1';
+        }
+        }, 4000); // Change image every 5 seconds
+    }
+    });
+</script>
