9d477ebdde1e22e4db958f8b42306de17445193a
Daniel-Digid <daniel@dig.id>
2024-10-01 16:24:46 +0100

    d

--- template-parts/pages/home/location.php
+++ template-parts/pages/home/location.php
@@ -12,8 +12,8 @@
             endif;
             ?>
         </div>
-        <div class="col-span-1 md:col-span-5 xl:col-span-3 col-start-2 md:col-start-7 xl:col-start-6 flex flex-col xl:justify-between md:pl-7 pt-[30%] md:pt-0">
-            <p class="font-miller font-normal text-sm md:text-lg xl:text-2xl tracking-[0.14px] md:tracking-[0.24px] text-blue-shade-2 mb-6 xl:mb-0"><?php echo get_field( 'location_text' ); ?></p>
+        <div class="col-span-1 md:col-span-5 xl:col-span-3 col-start-2 md:col-start-7 xl:col-start-6 flex flex-col lg:justify-between md:pl-7 pt-[30%] md:pt-0">
+            <p class="font-miller font-normal text-[0.875rem] text-s md:text-[1.125rem] xl:text-[1.5rem] tracking-[0.14px] leading-normal md:leading-tight md:tracking-[0.24px] text-blue-shade-2 mb-6 xl:mb-0"><?php echo get_field( 'location_text' ); ?></p>
             <?php 
             $location_btn = get_field('location_button');
             if( $location_btn ): 
@@ -21,7 +21,7 @@
                 $location_btn_title = $location_btn['title'];
                 $location_btn_target = $location_btn['target'] ? $location_btn['target'] : '_self';
                 ?>
-                <a class="btn-internal btn-internal--shade-4 !py-3 xl:!px-0 !font-light text-center !hidden md:!inline-block" href="<?php echo esc_url( $location_btn_url ); ?>" target="<?php echo esc_attr( $location_btn_target ); ?>"><?php echo esc_html( $location_btn_title ); ?></a>
+                <a class="btn-internal btn-custom--shade-4 !font-light text-center !hidden md:!inline-block" href="<?php echo esc_url( $location_btn_url ); ?>" target="<?php echo esc_attr( $location_btn_target ); ?>"><?php echo esc_html( $location_btn_title ); ?></a>
             <?php endif; ?>
         </div>
         <div class="col-span-2 md:hidden text-center">
@@ -32,7 +32,7 @@
                 $location_btn_title = $location_btn['title'];
                 $location_btn_target = $location_btn['target'] ? $location_btn['target'] : '_self';
                 ?>
-                <a class="btn-internal btn-internal--shade-4 !py-3 xl:!px-0 !font-light text-center" href="<?php echo esc_url( $location_btn_url ); ?>" target="<?php echo esc_attr( $location_btn_target ); ?>"><?php echo esc_html( $location_btn_title ); ?></a>
+                <a class="btn-internal btn-custom--shade-4 !font-light text-center" href="<?php echo esc_url( $location_btn_url ); ?>" target="<?php echo esc_attr( $location_btn_target ); ?>"><?php echo esc_html( $location_btn_title ); ?></a>
             <?php endif; ?>
         </div>
     </div>
