d8aa8d957eb5886638d5a538908ea4d735f95d9c
Daniel-Digid <daniel@dig.id>
2024-10-08 11:05:01 +0100

    d

--- template-parts/pages/home/hero.php
+++ template-parts/pages/home/hero.php
@@ -5,12 +5,19 @@
 			$hero_image = get_field('hero_section_hero_image');
 			$size = 'full';
 			if( $hero_image ) {
-				echo wp_get_attachment_image( $hero_image, $size, false, array( 'class' => 'w-full' ) );
+				echo wp_get_attachment_image( $hero_image, $size, false, array( 'class' => 'w-full hidden lg:block' ) );
+			} 
+		?>
+		<?php 
+			$hero_image = get_field('hero_section_hero_image_mobile');
+			$size = 'full';
+			if( $hero_image ) {
+				echo wp_get_attachment_image( $hero_image, $size, false, array( 'class' => 'w-full block lg:hidden max-h-none md:max-h-[75vh] lg:max-h-none' ) );
 			} 
 		?>
 		<div class="absolute bottom-0 left-0 w-full h-[35vh]" style="background: linear-gradient(180deg, rgba(14, 50, 74, 0.00) 0%, #0E324A 100%);"></div>
 	</div>
-	<div class="hero-title text-blue-shade-2 bg-blue-shade-5 -mt-[3px] pb-12 pt-12 lg:pt-0">
+	<div class="hero-title text-blue-shade-2 bg-blue-shade-5 -mt-28 lg:-mt-[3px] pb-12 pt-12 lg:pt-0">
 		<div class="grid grid-cols-2 xl:grid-cols-12">
 			<div class="col-span-2 xl:col-span-10 col-start-1 xl:col-start-2 flex flex-col xl:flex-row justify-center items-center text-center">
 				<p class="section-hero--title font-miller font-light text-base xl:text-2xl invisible"><?php echo get_field( 'hero_section_before_title' ); ?></p>
@@ -23,6 +30,12 @@
 		</div>
 		<div class="w-full text-center pt-7">
 			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/paper-boat.svg" class="section-hero--boat mx-auto w-[55px] xl:w-[83px] invisible" alt="Paper boat" title="Paper boat" />
+			<?php
+			$booking_url = get_field( 'booking_url', 'options' );
+			if ( $booking_url ) :
+				?><a href="<?php echo esc_url( $booking_url ); ?>" target="_blank" class="btn-header btn-header--booking uppercase mt-7 inline-block lg:hidden"><?php esc_html_e( 'Jetzt Buchen', 'aare' ); ?></a><?php
+			endif;
+			?>
 		</div>
 	</div>
 </section>
