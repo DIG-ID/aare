2ed44b887607c016dc017e11db22887fd5a79e8f
Daniel-Digid <daniel@dig.id>
2024-10-11 11:33:34 +0100

    d

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -9,9 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	//wait until images, links, fonts, stylesheets, and js is loaded
 	window.addEventListener("load", () => {
 
-		const isAboutPage = document.body.classList.contains('page-template-page-about');
-	
-		if (!isAboutPage) {
+			document.documentElement.style.scrollBehavior = 'auto';
 
 			// Initialize Lenis only if it's NOT the about page
 			const lenis = new Lenis({
@@ -34,12 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			});
 
 			gsap.ticker.lagSmoothing(0);
-		} else {
-			// Ensure the lenis classes and scroll behavior are removed on the about page
-			document.documentElement.classList.remove('lenis', 'lenis-scrolling', 'lenis-smooth');
-			document.documentElement.style.scrollBehavior = '';
-		}
-
+		
 		// GSAP horizontal scroll effect (this will still run on the about page)
 		gsap.registerPlugin(ScrollTrigger);
 		
