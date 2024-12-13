393d49d1b3562874a6fe159619b0308dd5aa34a9
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-12-13 16:53:27 +0000

    up

--- 404.php
+++ 404.php
@@ -2,27 +2,18 @@
 get_header();
 do_action( 'before_main_content' );
 ?>
-<section class="page-header bg-blue-shade-5 text-blue-shade-1 pt-28 lg:pt-64 pb-0 lg:-mb-[70px]">
-	<div class="theme-container">
-		<div class="flex flex-col w-full text-center">
-			<div class="page-header--title">
-				<h1 class="font-miller font-light text-5xl xl:text-[250px] text-blue-shade-2 py-5 lg:py-10 invisible"><?php echo esc_html__( '404', 'aare' ) ?></h1>
-			</div>
-		</div>
+<section class="page-header bg-blue-shade-5 text-blue-shade-1 pt-28 lg:pt-64">
+	<div class="theme-container flex flex-col w-full text-center">
+		<h2 class="text-title-h3 !font-light text-blue-shade-2 mb-12"><?php esc_html_e( 'Seite nicht gefunden.', 'aare' ); ?></h2>
+		<h1 class="font-miller font-light text-[10rem] xl:text-[15.625rem] text-blue-shade-2 leading-none relative xl:-bottom-7"><?php esc_html_e( '404', 'aare' ); ?></h1>
 	</div>
 </section>
-<section class="section-error-404 not-found bg-blue-shade-4 text-blue-shade-1 pt-32 pb-48">
+<section class="section-error-404 not-found bg-blue-shade-4 text-blue-shade-1 pt-12 lg:pt-32 pb-12 lg:pb-48">
 	<div class="theme-container text-center">
-            <h3 class="text-title-h3 !font-light text-blue-shade-2 mb-12"><?php esc_html_e( 'Page not found', 'aare' ) ?></h3>
-            <?php
-            $booking_url = get_field( 'booking_url', 'options' );
-            if ( $booking_url ) :
-                ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="btn-internal btn-internal--shade-4 !py-3 !px-32 !font-light tracking-[0.2px]"><?php esc_html_e( 'Homepage', 'aare' ); ?></a><?php
-            endif;
-            ?>
-    </div>
+		<h3 class="text-title-h3 !font-light text-blue-shade-2 mb-12"><?php esc_html_e( 'Die von Ihnen gesuchte Seite existiert nicht oder wurde verschoben.', 'aare' ); ?></h3>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="btn-internal btn-internal--shade-4 !py-3 !lg:px-32 !font-light tracking-[0.2px]"><?php esc_html_e( 'zurück zur Startseite', 'aare' ); ?></a>
+	</div>
 </section>
-
 <?php
 do_action( 'after_main_content' );
 get_footer();
